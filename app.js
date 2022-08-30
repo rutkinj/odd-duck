7238c799555062b6996ea484374639635103c84b
Joe Rutkin <rutkinj@gmail.com>
2022-08-29 17:17:07 -0700

    adds basic functionality to page

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 'use strict';
 console.log('Hello, world.');
 
-let prodCatalog = [];
+///////GLOBAL :( ///////
+let maxClicks = 25;
+let clicks = 0;
 
 //////// CONSTRUCTOR //////////
 // gotta be in /img/; gotta be .jpg;
+
 function Product(name){
   this.name = name;
   this.img = './img/' + name + '.jpg';
   this.shownCount = 0;
   this.chosenCount = 0;
 
-  prodCatalog.push(this);
+  Product.catalog.push(this);
 }
 
 /////////// PROTO ///////////
+
+Product.catalog = [];
+
 
 Product.prototype.incChosen = function (amount = 1){
   this.chosenCount += amount;
 };
 
+
 Product.prototype.incShown = function (){
   this.shownCount++;
 };
 
+
 ////////// PROD INIT //////////
+
 
 new Product('bag');
 new Product('banana');
@@ -46,18 +55,21 @@ new Product('unicorn');
 new Product('water-can');
 new Product('wine-glass');
 
+
 ////////// FUNCY TOWN //////////
 
+
 function randIndex(){
-  let randIdx = Math.floor(Math.random() * prodCatalog.length);
+  let randIdx = Math.floor(Math.random() * Product.catalog.length);
   return randIdx;
 }
+
 
 function randProducts(amount = 3){
   let arr = [];
 
   while (arr.length < amount){
-    let randyPro = prodCatalog[randIndex()];
+    let randyPro = Product.catalog[randIndex()];
     if(!arr.includes(randyPro)){
       arr.push(randyPro);
     }
@@ -66,18 +78,75 @@ function randProducts(amount = 3){
 }
 
 
-
-function render(array){
-  let imgContainer = document.getElementById('img-display');
-  console.log(imgContainer);
-  for(let i = 0; i < array.length; i++){
-    array[i].incShown();
-    console.log(array[i]);
-    let prodImg = document.createElement('img');
-    prodImg.src = array[i].img;
-    prodImg.alt = array[i].name;
-    imgContainer.appendChild(prodImg);
+function handleClick(event) {
+  console.log(event.target.alt);
+  clicks++;
+  let chosenProd = event.target.alt;
+  for (let i=0; i < Product.catalog.length; i++){
+    if (chosenProd === Product.catalog[i].name){
+      Product.catalog[i].incChosen();
+      render(randProducts());
+    }
   }
 }
+
+//---------- RESULTS ----------------------//
+
+
+function results(){
+  resultsList();
+  resultsImgs();
+}
+
+function resultsList(){
+  let resultsList = document.getElementById('results-list');
+  for (let i = 0; i < Product.catalog.length; i++) {
+    let prodLi = document.createElement('li');
+    let prod = Product.catalog[i];
+    let prodInfo = `Product: ${prod.name}. 
+                    Clicks: ${prod.chosenCount}. 
+                    Appearences: ${prod.shownCount}. `;
+    prodLi.textContent = prodInfo;
+    resultsList.appendChild(prodLi);
+  }
+}
+
+function resultsImgs(){
+  //doot doot tbd
+}
+let resultsListEle = document.getElementById('results-list');
+// resultsListEle.style.display = 'none';
+let resultsArea = document.getElementById('results');
+resultsArea.addEventListener('button', displayResults);
+
+function displayResults(){
+  console.log('bingbong');
+  resultsListEle.style.visibility = 'initial';
+}
+
+
+//----------RENDER---------------------//
+
+function render(array){
+
+  let imgContainer = document.getElementById('img-display');
+  while (imgContainer.firstChild){
+    imgContainer.removeChild(imgContainer.firstChild);
+  }
+  if (clicks < maxClicks) {
+    for(let i = 0; i < array.length; i++){
+      array[i].incShown();
+      let prodImg = document.createElement('img');
+      prodImg.src = array[i].img;
+      prodImg.alt = array[i].name;
+      prodImg.addEventListener('click', handleClick);
+      imgContainer.appendChild(prodImg);
+    }
+  } else {
+    results();
+    console.log('beep boop here is results');
+  }
+}
+
 
 render(randProducts());
